e654e0067472b74f3d490738993877988a812740
Shi Yan <billconan@gmail.com>
2022-11-03 22:33:32 -0700

    update

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,96 @@
+use std::env;
+use std::fs;
+use std::io::{self, Read};
+use toml::map::Map;
+use toml::Value;
+
+fn verify_chapter(content: &toml::map::Map<String, toml::Value>, indent: usize) -> bool {
+    if !content.contains_key("title") {
+        println!("Toml Format Error: A Chapter must have a title.");
+        return false;
+    }
+    else {
+        println!("{:indent$}{}","", content.get("title").unwrap(), indent=indent);
+    }
+
+    if !content.contains_key("folder") {
+        println!("Toml Format Error: A Chapter must have a folder.");
+        return false;
+    }
+
+    if content.contains_key("sub_chapters") {
+        if let toml::Value::Array(content) = content.get("sub_chapters").unwrap() {
+            for c in content {
+                if let toml::Value::Table(c) = c {
+                    if !verify_chapter(c, indent + 2) {
+                        return false;
+                    }
+                } else {
+                    println!("Toml Format Error: A sub chapter must be a map type.");
+                }
+            }
+            return true;
+        } else {
+            println!("Toml Format Error: Sub chapter field must have an array type.");
+        }
+    }
+
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    //https://patorjk.com/software/taag/#p=display&f=Ogre&t=Code%20Stage
+    println!(
+        "     ___          _        __ _                   
+    / __\\___   __| | ___  / _\\ |_ __ _  __ _  ___ 
+   / /  / _ \\ / _` |/ _ \\ \\ \\| __/ _` |/ _` |/ _ \\
+  / /__| (_) | (_| |  __/ _\\ \\ || (_| | (_| |  __/
+  \\____/\\___/ \\__,_|\\___| \\__/\\__\\__,_|\\__, |\\___|
+                                       |___/      "
+    );
+
+    let contents = fs::read_to_string("test.toml").expect("Should have been able to read the file");
+
+    let value = match contents.parse::<Value>() {
+        Err(error) => {
+            println!("Toml Parsing Error: {}", error.to_string());
+            return;
+        }
+        Ok(value) => value,
+    };
+
+    if let toml::Value::Table(ref global) = value {
+        if !global.contains_key("title") {
+            println!("Toml Format Error: Mandatory field \"title\" doesn't exist.");
+            return;
+        }
+
+        let content = match global.get("content") {
+            None => {
+                println!("Warning: No content detected.");
+                return;
+            }
+            Some(content) => content,
+        };
+
+        if let toml::Value::Array(ref content) = content {
+            for c in content {
+                if let toml::Value::Table(ref chapter) = c {
+                    if verify_chapter(chapter, 0) == false {
+                        return;
+                    }
+                } else {
+                    println!("Toml Format Error: A chapter needs to be a table format.");
+                }
+            }
+        }
+    } else {
+        println!("Toml Format Error: The input file doesn't contain configurations.");
+        return;
+    }
+
+    //println!("{:?}", value);
+
+    serde_json::to_writer_pretty(io::stdout(), &value).unwrap();
+    //assert_eq!(value["foo"].as_str(), Some("bar"));
 }
